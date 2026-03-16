aa4dbe5d280c4d38636439d1a04763b92c732c3e
MiguelM-criptografia <miguelpereiramedeiros697@gmail.com>
2026-03-16 16:13:04 -0300

    com

--- funcoes.php
+++ funcoes.php
@@ -5,4 +5,10 @@ function soma($n1, $n2){
 function subtracao($n1, $n2){
     return($n1-$n2);
 }
+function divisao($n1, $n2){
+    return($n1/$n2);
+}
+function multiplicacao($n1, $n2){
+    return($n1*$n2);
+}
 ?>
