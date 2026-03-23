a9461fe0ffb8072fb83ed247f6a39ad5f1e29535
MiguelM-cryp <miguelpereiramedeiros697@gmail.com>
2026-03-23 14:13:41 -0300

    correto

--- funcoes.php
+++ funcoes.php
@@ -6,7 +6,9 @@ function subtracao($n1, $n2){
     return($n1-$n2);
 }
 function divisao($n1, $n2){
+    if($num2 !=0)
     return($n1/$n2);
+else return ("Erro: Não pode dividir por zero");
 }
 function multiplicacao($n1, $n2){
     return($n1*$n2);
